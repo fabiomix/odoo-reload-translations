4c2e1e9b952dd8d6ad168cd4bc760e8755d4dbb8
fabiomix <fabiomix@users.noreply.github.com>
2018-04-22 11:08:34 +0200

    create wizard

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Reload translations",
+    'summary': "Reload i18n files",
+
+    'description': """
+    """,
+
+    'author': "fabiomix",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '10.0.1.0.0',
+
+    # any module necessary for this one to work correctly 
+    'depends': ['base'],
+
+    'data': [
+
+        # wizards
+        'wizard/reload_translations.xml',
+
+    ],
+
+    'application': False,
+    'installable': True
+}
---
+++ wizard/reload_translations.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError, UserError
+
+import pprint
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class ReloadTranslations(models.TransientModel):
+	_name = 'wizard.reload_translations'
+
+
+	def get_language_list(self):
+		active_languages = self.env['res.lang'].search([
+				('translatable', '=', True), 
+				('active', '=', True)
+			])
+
+		return [(l.code, l.name) for l in active_languages]
+
+	
+	lang = fields.Selection(
+			selection=get_language_list, 
+			string="Language",
+			required=True
+		)
+
+	module_ids = fields.Many2many(
+			comodel_name='ir.module.module', 
+			relation='wizard_reload_translation_module_rel', 
+			column1='wizard_id', 
+			column2='module_id',
+			string='Modules', 
+			domain=[('state','=','installed')]
+		)
+
+
+	def do_reload_translation(self):
+		pass
