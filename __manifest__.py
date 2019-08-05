714353a8f0af09f0cf630d6941bb5773c7681443
fabiomix <fabiomix@users.noreply.github.com>
2019-08-05 16:47:37 +0200

    update module version, description and category

--- __manifest__.py
+++ __manifest__.py
@@ -2,18 +2,18 @@
 {
     'name': "Reload translations",
     'summary': "Reload i18n files",
-
     'description': """
+        Reload translation files of Odoo modules.
     """,
 
     'author': "fabiomix",
     'website': "",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # Check https://github.com/OCA/OCB/blob/10.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Extra Tools',
-    'version': '10.0.1.0.0',
+    'category': 'Localization',
+    'version': '10.0.1.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
